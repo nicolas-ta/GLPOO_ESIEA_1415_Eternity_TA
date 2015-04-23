0645b0320d4d302c63b28382b1448a69c9763566
Gabriel <charlesgnanesw@et.esiea.fr>
2015-04-23 16:13:59 +0200

    les fichiers de départ

---
+++ src/main/java/Initialisation/Motif.java
@@ -0,0 +1,23 @@
+package Initialisation;
+
+import java.io.File;
+
+
+public enum Motif {
+	MOTIF_0("NOIR", null),MOTIF_1("BLANC", null),MOTIF_2("JAUNE", null),MOTIF_3("ORANGE", null),MOTIF_4("BLEU", null);
+	private String symbole;
+	private File f;
+	
+	Motif(String symbole, File f){
+		this.symbole= symbole;
+		this.f=f;
+	}
+	
+	public String getSymbole(){
+		return this.symbole;
+	}
+	
+	public File getFichier(){
+		return this.f;
+	}
+}
---
+++ src/main/java/Initialisation/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Gabriel
+ *
+ */
+package Initialisation;
