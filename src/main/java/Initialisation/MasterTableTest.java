39b98abcdcf693e30f5a5f0c9e309baa7033a5e4
Gabriel <charlesgnanesw@et.esiea.fr>
2015-04-30 18:46:38 +0200

    C'est bon j'ai rentré en dure un premier plateau de jeu initialisé pour que vous puissez continuer à faire la suite car je galère à faire l'initialisation. La ce plateau est copier d'un trouvé sur internet donc il est bon.

---
+++ src/main/java/Initialisation/MasterTableTest.java
@@ -0,0 +1,106 @@
+package Initialisation;
+
+public class MasterTableTest {
+	
+	public Piece masterTableTest[][] = new Piece[4][4];
+	//J'initialise tout le tableau à la "main" c-a-d en retnrant toute les valeurs du tableau une par une 
+	public void fctInit(){
+		//
+		
+		//Piece 0 0
+		masterTableTest[0][0].motif[0] = Motif.MOTIF_0;
+		masterTableTest[0][0].motif[1] = Motif.MOTIF_0;
+		masterTableTest[0][0].motif[2] = Motif.MOTIF_2;
+		masterTableTest[0][0].motif[3] = Motif.MOTIF_1;
+		//Piece 0 1
+		masterTableTest[0][1].motif[0] = Motif.MOTIF_0;
+		masterTableTest[0][1].motif[1] = Motif.MOTIF_1;
+		masterTableTest[0][1].motif[2] = Motif.MOTIF_3;
+		masterTableTest[0][1].motif[3] = Motif.MOTIF_1;
+		//Piece 0 2
+		masterTableTest[0][2].motif[0] = Motif.MOTIF_0;
+		masterTableTest[0][2].motif[1] = Motif.MOTIF_1;
+		masterTableTest[0][2].motif[2] = Motif.MOTIF_4;
+		masterTableTest[0][2].motif[3] = Motif.MOTIF_1;
+		//Piece 0 3
+		masterTableTest[0][3].motif[0] = Motif.MOTIF_0;
+		masterTableTest[0][3].motif[1] = Motif.MOTIF_1;
+		masterTableTest[0][3].motif[2] = Motif.MOTIF_2;
+		masterTableTest[0][3].motif[3] = Motif.MOTIF_0;
+				
+		
+		//Piece 1 0
+		masterTableTest[1][0].motif[0] = Motif.MOTIF_2;
+		masterTableTest[1][0].motif[1] = Motif.MOTIF_0;
+		masterTableTest[1][0].motif[2] = Motif.MOTIF_1;
+		masterTableTest[1][0].motif[3] = Motif.MOTIF_3;
+		//Piece 1 1
+		masterTableTest[1][1].motif[0] = Motif.MOTIF_3;
+		masterTableTest[1][1].motif[1] = Motif.MOTIF_3;
+		masterTableTest[1][1].motif[2] = Motif.MOTIF_4;
+		masterTableTest[1][1].motif[3] = Motif.MOTIF_3;
+		//Piece 1 2
+		masterTableTest[1][2].motif[0] = Motif.MOTIF_4;
+		masterTableTest[1][2].motif[1] = Motif.MOTIF_3;
+		masterTableTest[1][2].motif[2] = Motif.MOTIF_3;
+		masterTableTest[1][2].motif[3] = Motif.MOTIF_4;
+		//Piece 1 3
+		masterTableTest[1][3].motif[0] = Motif.MOTIF_2;
+		masterTableTest[1][3].motif[1] = Motif.MOTIF_4;
+		masterTableTest[1][3].motif[2] = Motif.MOTIF_2;
+		masterTableTest[1][3].motif[3] = Motif.MOTIF_0;
+
+		
+		//Piece 2 0
+		masterTableTest[2][0].motif[0] = Motif.MOTIF_1;
+		masterTableTest[2][0].motif[1] = Motif.MOTIF_0;
+		masterTableTest[2][0].motif[2] = Motif.MOTIF_2;
+		masterTableTest[2][0].motif[3] = Motif.MOTIF_3;
+		//Piece 2 1
+		masterTableTest[2][1].motif[0] = Motif.MOTIF_4;
+		masterTableTest[2][1].motif[1] = Motif.MOTIF_3;
+		masterTableTest[2][1].motif[2] = Motif.MOTIF_3;
+		masterTableTest[2][1].motif[3] = Motif.MOTIF_4;
+		//Piece 2 2
+		masterTableTest[2][2].motif[0] = Motif.MOTIF_3;
+		masterTableTest[2][2].motif[1] = Motif.MOTIF_4;
+		masterTableTest[2][2].motif[2] = Motif.MOTIF_4;
+		masterTableTest[2][2].motif[3] = Motif.MOTIF_4;
+		//Piece 2 3
+		masterTableTest[2][3].motif[0] = Motif.MOTIF_2;
+		masterTableTest[2][3].motif[1] = Motif.MOTIF_4;
+		masterTableTest[2][3].motif[2] = Motif.MOTIF_1;
+		masterTableTest[2][3].motif[3] = Motif.MOTIF_0;
+
+		
+		//Piece 3 0
+		masterTableTest[3][0].motif[0] = Motif.MOTIF_2;
+		masterTableTest[3][0].motif[1] = Motif.MOTIF_0;
+		masterTableTest[3][0].motif[2] = Motif.MOTIF_0;
+		masterTableTest[3][0].motif[3] = Motif.MOTIF_2;
+		//Piece 3 1
+		masterTableTest[3][1].motif[0] = Motif.MOTIF_3;
+		masterTableTest[3][1].motif[1] = Motif.MOTIF_2;
+		masterTableTest[3][1].motif[2] = Motif.MOTIF_0;
+		masterTableTest[3][1].motif[3] = Motif.MOTIF_2;
+		//Piece 3 2
+		masterTableTest[3][2].motif[0] = Motif.MOTIF_4;
+		masterTableTest[3][2].motif[1] = Motif.MOTIF_2;
+		masterTableTest[3][2].motif[2] = Motif.MOTIF_0;
+		masterTableTest[3][2].motif[3] = Motif.MOTIF_1;
+		//Piece 3 3
+		masterTableTest[3][3].motif[0] = Motif.MOTIF_1;
+		masterTableTest[3][3].motif[1] = Motif.MOTIF_1;
+		masterTableTest[3][3].motif[2] = Motif.MOTIF_0;
+		masterTableTest[3][3].motif[3] = Motif.MOTIF_0;
+		
+		
+	
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
