13420ef41b2648d7034c44f2c60d24d5856d1047
hassanpourcent <gull@et.esiea.fr>
2015-05-15 23:52:31 +0200

    Update Piece.java

--- src/main/java/Jeu/Piece.java
+++ src/main/java/Jeu/Piece.java
@@ -23,7 +23,7 @@ public class Piece extends JComponent {
 		faces[3] = faceSudPiece;
 	}	
 	public Piece(int s){
-//		System.out.println("Creation d'une piece vide");		
+	
 		faces[0] = 0;
 		faces[1] = 0;
 		faces[2] = 0;
@@ -34,7 +34,7 @@ public class Piece extends JComponent {
 	}
 
 	public Piece(int s,int faceOuestPiece, int faceNordPiece, int faceEstPiece, int faceSudPiece){
-//		System.out.println("Creation d'une piece complete");		
+	
 		faces[0] = faceOuestPiece;
 		faces[1] = faceNordPiece;
 		faces[2] = faceEstPiece;
@@ -43,7 +43,7 @@ public class Piece extends JComponent {
 		repaint();		
 	}
 		
-	//SWAP FUNCTION
+
 	public void Echange(Piece piece2)	{
 		int swap;
 		
@@ -56,7 +56,6 @@ public class Piece extends JComponent {
 		
 	}
 	
-
 	
 	public void rotationDroite(){
 		int temp = faces[0];
@@ -67,54 +66,46 @@ public class Piece extends JComponent {
 		rotate = (rotate-1)%4;
 	}
 
-	
-	public void rotationGauche(){
-		int temp = faces[0];
-		faces[0]=faces[1];
-		faces[1]=faces[2];
-		faces[2]=faces[3];
-		faces[3]=temp;
-		rotate = (rotate+1)%4;
-	}	
-
 	public int Prendre(int nb){
 		return faces[nb];
 	}
 	
 
-	//////////////////////////////////////////////////////////////FONCTION POUR LE COLORIAGE/////////////////////////////////////////////////////
-	public void paintComponent(Graphics g) {
-	    g.setColor(Color.black);
-	    g.fillRect(0, 0, 100, 100);	    
-	    build(g);	    
+	public void paintComponent(Graphics g) {   
+	  build(g);	    
 	}
 	
 	private void setColorFace(int face, Graphics g){
-		 switch (face) {
-	         case 0: 
-	        	 	g.setColor(Color.white);
-	                break;
-	         case 1:  
-	        	 	g.setColor(Color.black);
-	         		break;
-	         case 2:  
-	        	 	g.setColor(Color.cyan);
-	         		break;
-	         case 3:  
-	        	 	g.setColor(Color.green);
-	         		break;
-	         case 4:  
-	        	 	g.setColor(Color.orange);
-	         		break;
-	         case 5: 
-	        	 	g.setColor(Color.pink);
-	         		break;
-	         default:
-	        	 	g.setColor(Color.black);
-	         		break;
-		 }	 
+		
+		   switch (face) {
+		          case 0: 
+		        
+		            g.setColor(Color.white);
+		                 break;
+		          case 1:  
+		        	
+		            g.setColor(Color.black);
+		            break;
+		          case 2:  
+		        	 
+		            g.setColor(new Color(54,156,228));
+		            break;
+		          case 3:  
+		            g.setColor(new Color(54,228,185));
+		            break;
+		          case 4:  
+		            g.setColor(new Color(225,228,54));
+		            break;
+		          case 5: 
+		            g.setColor(new Color(210,112,182));
+		            break;
+		          default:
+		            g.setColor(Color.black);
+		            break;
+		   }  
+		 
+		 }
 	
-	}
 	
 	public void update() {
 		repaint();		    
@@ -122,12 +113,13 @@ public class Piece extends JComponent {
 	
 
 	private void build(Graphics g){
-		
+	
 		setColorFace(faces[1], g);
 		Polygon p1 = new Polygon();
 		p1.addPoint(0,0);
 		p1.addPoint(size.x,0);
-		p1.addPoint(size.x/2,size.y/2);        
+		p1.addPoint(size.x/2,size.y/2); 
+	
 		((Graphics2D) g).fill(p1);
 		
 	    
@@ -142,7 +134,8 @@ public class Piece extends JComponent {
 	    Polygon p3 = new Polygon();
         p3.addPoint(size.x,0);
         p3.addPoint(size.x,size.y);
-        p3.addPoint(size.x/2,size.y/2);        
+        p3.addPoint(size.x/2,size.y/2); 
+  
 	    ((Graphics2D) g).fill(p3);
 	    
 		setColorFace(faces[3], g);
@@ -154,21 +147,7 @@ public class Piece extends JComponent {
 		
 	}
 	
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-	/*
-	public void FramePiece(Graphics g){
-		  Polygon f = new Polygon();
-	      f.addPoint(0,size.y);
-	      f.addPoint(0,0);
-	      f.addPoint(size.x,0);
-	      f.addPoint(size.x,size.y);
-	      ((Graphics2D) g).draw(f);		
-	}
 
-	*/
-	
-	
 	
 	
 }
