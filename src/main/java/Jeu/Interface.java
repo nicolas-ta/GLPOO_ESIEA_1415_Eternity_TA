7e695c599f8aa3658a04e466511d25b4797653df
BadBoy <gull@et.esiea.fr>
2015-05-15 16:03:24 +0200

    Fix Interface BUG

--- src/main/java/Jeu/Interface.java
+++ src/main/java/Jeu/Interface.java
@@ -1,6 +1,8 @@
 package Jeu;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -11,25 +13,17 @@ import java.awt.event.MouseListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
-import javax.swing.ButtonGroup;
-import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JRadioButtonMenuItem;
 
 public class Interface extends JFrame implements KeyListener,ActionListener,MouseListener{
 	
 	
-	private int size = 4;
+
 	
-	private static int score=9999;
+	
 	private JPanel panel;
 	private JFrame frame;
-	private JButton begin;
 	private JButton restart;
 	private JButton Rotate;
 	private JLabel time = new JLabel("00:00");
@@ -71,18 +65,18 @@ public class Interface extends JFrame implements KeyListener,ActionListener,Mous
  	}	
 	
 
-	private void init_terrain(int x, int y){
+	private void init_terrain(){
 		
-		PlateauJeu = new JButton[size][size];
-	    Terrain = new Plateau(size,size,100);	
+		PlateauJeu = new JButton[4][4];
+	    Terrain = new Plateau(4,4,100);	
 		
 		//Creation des bouton des piece du plateau
-		for(int i=0; i<x ; i++){
-			for(int j=0; j<y ; j++){	
+		for(int i=0; i<4 ; i++){
+			for(int j=0; j<4 ; j++){	
 				//bouton
 				PlateauJeu[i][j] = new JButton();			    
 
-				PlateauJeu[i][j].setBounds(5+5*i+100*i, 5+5*j+100*j, 100,100);
+				PlateauJeu[i][j].setBounds(55+5*i+90*i, 205+5*j+90*j, 100,100);
 				PlateauJeu[i][j].addActionListener(this); 
 				PlateauJeu[i][j].setContentAreaFilled(false); 
 				PlateauJeu[i][j].addMouseListener(this); 
@@ -90,17 +84,17 @@ public class Interface extends JFrame implements KeyListener,ActionListener,Mous
 				panel.add(PlateauJeu[i][j]);
 				
 				
-				Terrain.pieces[i][j].setBounds(5+5*i+100*i, 5+5*j+100*j, 100,100);	
+				Terrain.pieces[i][j].setBounds(55+5*i+90*i, 205+5*j+90*j, 100,100);	
 				panel.add(Terrain.pieces[i][j]);
 			}
 		}		
 	}
 	
 	
-	private void init_reserve(int x, int y){
+	private void init_reserve(){
 		
-		Reserve = new Plateau(size,size,50);	
-		Puzzle = new JButton[size][size];
+		Reserve = new Plateau(4,4,65);	
+		Puzzle = new JButton[4][4];
 			
 		
 		
@@ -108,21 +102,21 @@ public class Interface extends JFrame implements KeyListener,ActionListener,Mous
 		Reserve.Lecture();
 		
 		//Creation des bouton des pieces du Reserve
-	    for(int i=0; i<x ; i++){
+	    for(int i=0; i<4 ; i++){
 			
-	    	for(int j=0; j<y ; j++){		
+	    	for(int j=0; j<4 ; j++){		
 				
 				//bouton
 				
 				Puzzle[i][j] = new JButton();			
-				Puzzle[i][j].setBounds(440+5*i+50*i, 205+5*j+50*j, 50,50); 
+				Puzzle[i][j].setBounds(580+55*i+50*i, 205+55*j+50*j, 65,65); 
 				Puzzle[i][j].addActionListener(this); 
 			    Puzzle[i][j].setContentAreaFilled(false);	
 			    Puzzle[i][j].addMouseListener(this); 	
 			    panel.add(Puzzle[i][j]);
 			    
 			   
-			    Reserve.pieces[i][j].setBounds(440+5*i+50*i, 205+5*j+50*j, 50,50);	
+			    Reserve.pieces[i][j].setBounds(580+55*i+50*i, 205+55*j+50*j, 65,65);	
 			    panel.add(Reserve.pieces[i][j]);
 			}
 		
@@ -134,60 +128,69 @@ public class Interface extends JFrame implements KeyListener,ActionListener,Mous
 private void Debut(){	
 		
 		//Creation de la fenetre
-
+	//	System.out.println("Creation de la fenetre");	
 		frame = new JFrame("Eternity Game");  
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 	    
 	    panel = (JPanel)frame.getContentPane();  
 	    panel.setLayout(null);
+	
+		panel.setBackground(new Color(186,44,44));
 	    
+	   // panel.setBackground(Color.BLUE);
 	    //Creation des object graphique
 	//    System.out.println("Creation des object graphique");	
 	    
 
-		Font font = new Font("Arial",Font.BOLD,33);
+		Font font = new Font("Fixedsys", Font.BOLD,20);
 		time.setFont(font);		
-		time.setBounds(505,30,500,30);	
+		time.setBounds(493,30,500,30);	
 	
-	// Start
-		begin = new JButton("Commencer");
-	    begin.addActionListener(this);
-		begin.setBounds(440, 150, 215,30);
-		
 	// Option qu'on devra integrer dans le menu on mettant une touche restart par exemple
 		//bouton restart
-	    restart = new JButton("Recommencer la partie");	 
+		Graphics g;
+	    restart = new JButton("Recommencer la partie");	
+	    Font font2 = new Font("Fixedsys", Font.BOLD,13);
         restart.addActionListener(this);
-        restart.setBounds(440, 85, 215,30);
+        restart.setFont(font2);
+        restart.setBounds(412, 85, 215,30);
         
         //On ajoute tout sa a la page
         panel.add(time);
-        panel.add(restart);             
-        panel.add(begin);
+        panel.add(restart);               
       
 	}
 	
 
+	private void setDisplayColor(Color color) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+	private void setPenColor(Color color) {
+	// TODO Auto-generated method stub
+	
+}
+
+
 	public Interface() {
 		
 		//Debut nouvelle partie
-
+	//	System.out.println("Debut nouvelle partie");	
 		Debut();
-		new Menu();
 		
-	   init_terrain(size,size);
-	init_reserve(size,size);
-        
-        
-        frame.setSize(677, 463);
+		
+	    init_terrain();
+	    init_reserve();
+
+        frame.setSize(1024, 768);
 		frame.setVisible(true);	
+		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
-	    frame.setLocationRelativeTo(null);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setResizable(false);
-	    setAlwaysOnTop(false);
-	    frame.setContentPane(panel);
-	    frame.setVisible(true);	
+		
+				
 		timer1 = new Timer(1000, action_timer);		
+	
 		timer1.start();     
 		
 	}
@@ -196,15 +199,7 @@ private void Debut(){
 	
 	//Evenement	
 	private Piece last=null;
-	public void actionPerformed(ActionEvent e) {	
-		
-		
-		//Pour débuter la partie
-		/*if(e.getSource() == begin){
-			
-			 init_reserve(size,size);
-		
-		}*/
+	public void actionPerformed(ActionEvent e) {			
 
 		
 		//Pour chaque piece du Terrain
@@ -268,9 +263,9 @@ private void Debut(){
 		
 	}
 
-	/*public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent arg0) {
 		int key = arg0.getKeyCode();	
-	}*/
+	}
 
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub		
@@ -350,84 +345,7 @@ private void Debut(){
 				}
 
 	}
-	
-	
-	
-/*	public class Menu extends JFrame {
-		  private JMenuBar menuBar = new JMenuBar();
-		  private JMenu Jeu = new JMenu("Jeu");
-		  private JMenu Highscores = new JMenu("Highscores");
-
-		  private JMenuItem Nouvelle = new JMenuItem("Nouvelle partie");
-		  private JMenuItem Save = new JMenuItem("Sauvegarder la partie");
-		  private JMenuItem Load = new JMenuItem("Charger la partie");
-		  private JMenuItem Quit = new JMenuItem("Quitter le jeu");
-		  
-			
-
-		  public void main(String[] args){
-		    Menu zFen = new Menu();
-		  }
-
-		  public Menu(){
-
-
-		    //On initialise nos menus      
-		    this.Jeu.add(Nouvelle);
-
-
-		    Quit.addActionListener(new ActionListener(){
-		      public void actionPerformed(ActionEvent arg0) {
-		        System.exit(0);
-		      }
-		      
-		    });
-		*/    
-	/*	    Save.addActionListener(new ActionListener(){
-		        public void actionPerformed(ActionEvent arg0) {
-		        	MasterTable.fctSauvegarder();
-		        }
-		        
-		      }); */
-		    
-		/*    Load.addActionListener(new ActionListener(){
-		        public void actionPerformed(ActionEvent arg0) {
-		        	MasterTable.fctRestaurer();
-		        }
-		        
-		      }); 
-		    */
-		    
-		/*    this.Jeu.add(Save);  
-		    this.Jeu.add(Load);
-		    this.Jeu.addSeparator();
-		    this.Jeu.add(Quit);
-
-		    //L'ordre d'ajout va d�terminer l'ordre d'apparition dans le menu de gauche � droite
-		    //Le premier ajout� sera tout � gauche de la barre de menu et inversement pour le dernier
-		    this.menuBar.add(Jeu);
-		    this.menuBar.add(Highscores);
-		    this.setJMenuBar(menuBar);
-		    
-		    this.setTitle("Eternity Game");
-		    this.setSize(1280, 768);
-		    this.setLocationRelativeTo(null);
-		    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    setResizable(false);
-		    setAlwaysOnTop(false);
-		    //Instanciation d'un objet JPanel
-		  //  JPanel pan = new JPanel();  
-		//    pan.add(new Panneau());
-		//  pan.add(new PanneauScore());
-		//    pan.add(new PanneauDebut());
-		    
-
-		    
-		//    this.setContentPane(pan);
-		 //   this.setVisible(true);
-		  }
-		}
-
-
-*/
 }
+	
+	
+	
