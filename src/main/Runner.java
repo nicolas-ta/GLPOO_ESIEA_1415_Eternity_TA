d75c44f119918a0c566e70b06c24da838281b717
BadBoy <gull@et.esiea.fr>
2015-05-15 15:13:01 +0200

    La nouvelle cam

---
+++ src/main/Runner.java
@@ -0,0 +1,12 @@
+package Jeu;
+
+public class Runner  {
+	
+	public static void main(String[] args)  {
+		
+		System.out.println("Lancement du programme");
+		new Interface();		
+		//new Menu();
+
+	}
+}
